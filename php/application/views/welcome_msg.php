9f472ad78b27eb7482b80e1d385f572ed346980b
tquangdo <trangia61@gmail.com>
2020-11-22 03:07:48 +0900

    first commit

---
+++ php/application/views/welcome_msg.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="icon" href="https://www.iconfinder.com/data/icons/most-usable-logos/120/Amazon-512.png?w=800&h=800" />
+	<title>DoTQ App</title>
+</head>
+<body>
+<div id="app"></div>
+  <script type="text/javascript" src="<?= base_url('react_app/dist/index_bundle.js') ?>" ></script>
+</body>
+</html>
